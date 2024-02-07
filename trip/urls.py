da43f560d0bdc94624b9b9589faaeb7aede2b238
Gabriel Nathan <121491867+nythan5@users.noreply.github.com>
2024-02-06 23:55:31 -0300

    Template Create Category

---
+++ trip/urls.py
@@ -0,0 +1,8 @@
+# trip/urls.py
+from django.urls import path
+from .views import create_category
+
+urlpatterns = [
+    path('create_category/', create_category, name='create_category'),
+    # Adicione outras URLs conforme necessário
+]
--- trip/views.py
+++ trip/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .forms import CategoryForm
 # Create your views here.
+
+
+def create_category(request):
+    form = CategoryForm()
+    return render(request, 'trip/pages/create_category.html', {'form': form})
